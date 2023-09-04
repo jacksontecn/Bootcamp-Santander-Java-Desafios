60236a3a5bdbf479c55f721dc77f5029ce2e8acf
jacksontecn <arakisu@gmail.com>
2023-09-04 16:33:50 -0300

    Commitando desafio do grande deposito

---
+++ Desafios/src/Deposito.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Deposito {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double valor = scanner.nextDouble();
+
+        if (valor > 0) {
+            //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+            System.out.printf("Deposito realizado com sucesso! \nSaldo atual: R$ %.2f", valor);
+
+        } else if (valor == 0) {
+            //TODO: Imprimir a mensagem de valor inválido.
+            System.out.println("Encerrando o programa...");
+
+        } else {
+            //TODO: Imprimir a mensagem de encerrar o programa.
+            System.out.println("Valor invalido! Digite um valor maior que zero.");
+        }
+
+    }
+
+}
